c9c05083266467dd989cb0cdb992a86dfa054f49
jiujiuwo <lihaifeng94@qq.com>
2023-01-05 17:06:29 +0800

    用栈的数据结构来解决下一题

---
+++ src/top100/Top100_394.java
@@ -0,0 +1,13 @@
+package top100;
+
+/**
+ * 394. 字符串解码
+ * 初步判断用栈来做？
+ */
+public class Top100_394 {
+    class Solution {
+        public String decodeString(String s) {
+            return null;
+        }
+    }
+}
