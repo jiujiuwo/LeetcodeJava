914df758e80b06949fe5d3548a8c6bec45962fbb
jiujiuwo <lihaifeng94@qq.com>
2023-01-06 12:04:29 +0800

    看看能不能自己做出来

--- src/top100/Top100_394.java
+++ src/top100/Top100_394.java
@@ -1,5 +1,7 @@
 package top100;
 
+import java.util.Stack;
+
 /**
  * 394. 字符串解码
  * 初步判断用栈来做？
@@ -7,6 +9,19 @@ package top100;
 public class Top100_394 {
     class Solution {
         public String decodeString(String s) {
+            String result = "";
+            if (null == s || s.length() == 0) {
+                return result;
+            }
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+                if (ch == '[') {
+                    stack.push(ch);
+                }else if(ch >= 'a'&& ch <='z'){
+
+                }
+            }
             return null;
         }
     }
