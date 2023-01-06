2a7424a7816d3fc7220bb1936f99e838fbd92a97
jiujiuwo <lihaifeng94@qq.com>
2023-01-06 14:20:43 +0800

    看看能不能自己做出来

--- src/top100/Top100_394.java
+++ src/top100/Top100_394.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 /**
  * 394. 字符串解码
  * 初步判断用栈来做？
+ * 想到了用栈来操作，但是没有实现，我想的是每个字母出栈以后怎么做，实际应该是等整个[]出栈,然后再弹出一位必是数字
  */
 public class Top100_394 {
     class Solution {
@@ -13,14 +14,22 @@ public class Top100_394 {
             if (null == s || s.length() == 0) {
                 return result;
             }
-            Stack<Character> stack = new Stack<>();
-            for (int i = 0; i < s.length(); i++) {
+            String tmp = String.valueOf(0);
+            for (int i = 0; i < s.length(); ) {
                 char ch = s.charAt(i);
-                if (ch == '[') {
-                    stack.push(ch);
-                }else if(ch >= 'a'&& ch <='z'){
-
+                //如果是字母，直接追加
+                if (ch >= 'a' && ch <= 'z') {
+                    result += ch;
+                    i++;
+                    continue;
                 }
+                //如果是数字，则扫描后续内容入栈，直到遇到第一个']'
+                if (ch >= '0' && ch <= '9') {
+                    tmp += ch;
+                    i++;
+                    continue;
+                }
+                //如果是[,则
             }
             return null;
         }
