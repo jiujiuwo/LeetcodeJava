e899a8f95675fdf3cf4bfff16b3752b0a9166791
jiujiuwo <lihaifeng94@qq.com>
2023-01-06 16:13:30 +0800

    勉强算是用栈解决了字符串解码问题

--- src/top100/Top100_394.java
+++ src/top100/Top100_394.java
@@ -1,5 +1,6 @@
 package top100;
 
+import java.util.LinkedList;
 import java.util.Stack;
 
 /**
@@ -8,30 +9,95 @@ import java.util.Stack;
  * 想到了用栈来操作，但是没有实现，我想的是每个字母出栈以后怎么做，实际应该是等整个[]出栈,然后再弹出一位必是数字
  */
 public class Top100_394 {
-    class Solution {
-        public String decodeString(String s) {
-            String result = "";
-            if (null == s || s.length() == 0) {
-                return result;
-            }
-            String tmp = String.valueOf(0);
-            for (int i = 0; i < s.length(); ) {
-                char ch = s.charAt(i);
-                //如果是字母，直接追加
-                if (ch >= 'a' && ch <= 'z') {
-                    result += ch;
-                    i++;
-                    continue;
-                }
-                //如果是数字，则扫描后续内容入栈，直到遇到第一个']'
-                if (ch >= '0' && ch <= '9') {
-                    tmp += ch;
-                    i++;
-                    continue;
-                }
-                //如果是[,则
-            }
-            return null;
+
+    /**
+     * 通过率  29 / 34，不清楚哪里还有问题。。。
+     *
+     * @param s
+     * @return
+     */
+    public String decodeString(String s) {
+        String result = "";
+        String tmpResult = "";
+        if (null == s || s.length() == 0) {
+            return result;
         }
+        int multi = 0;
+        String stackS = "";
+        Stack<Integer> intS = new Stack<>();
+        Stack<String> strStack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '[') {
+                intS.push(multi);
+                if (stackS.length() > 0)
+                    strStack.push(stackS);
+                multi = 0;
+                stackS = "";
+            } else if (ch == ']') {
+                int mul = intS.pop();
+                String tmpS = "";
+                if (!strStack.isEmpty()) {
+                    tmpS = strStack.pop();
+                }
+                if (stackS.length() > 0) {
+                    tmpResult += tmpS;
+                    for (int j = 0; j < mul; j++) {
+                        tmpResult += stackS;
+                    }
+                } else {
+                    for (int j = 0; j < mul; j++) {
+                        tmpResult += tmpS;
+                    }
+                }
+                strStack.push(tmpResult);
+                stackS = "";
+                tmpResult = "";
+            } else if (ch >= '0' && ch <= '9') {
+                multi = 10 * multi + Integer.parseInt(ch + "");
+            } else {
+                stackS += ch;
+            }
+            if (i == s.length() - 1) {
+                strStack.push(stackS);
+            }
+        }
+        while (!strStack.isEmpty()) {
+            result = strStack.pop() + result;
+        }
+        return result;
+    }
+
+    /**
+     * 别人的100%的解法。
+     *
+     * @param s
+     * @return
+     */
+    public String decodeString1(String s) {
+        StringBuilder res = new StringBuilder();
+        int multi = 0;
+        LinkedList<Integer> stack_multi = new LinkedList<>();
+        LinkedList<String> stack_res = new LinkedList<>();
+        for (Character c : s.toCharArray()) {
+            if (c == '[') {
+                stack_multi.addLast(multi);
+                stack_res.addLast(res.toString());
+                multi = 0;
+                res = new StringBuilder();
+            } else if (c == ']') {
+                StringBuilder tmp = new StringBuilder();
+                int cur_multi = stack_multi.removeLast();
+                for (int i = 0; i < cur_multi; i++) tmp.append(res);
+                res = new StringBuilder(stack_res.removeLast() + tmp);
+            } else if (c >= '0' && c <= '9') multi = multi * 10 + Integer.parseInt(c + "");
+            else res.append(c);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String result = new Top100_394().decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef");
+        System.out.println(result);
     }
 }
