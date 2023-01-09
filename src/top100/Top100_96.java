8e8cc3707139e14e91f6abb5034b51ff3a61cfdb
jiujiuwo <lihaifeng94@qq.com>
2023-01-09 13:38:54 +0800

    动态规划法，解决不同二叉搜索树问题

---
+++ src/top100/Top100_96.java
@@ -0,0 +1,19 @@
+package top100;
+
+/**
+ * 96. 不同的二叉搜索树
+ * 动态规划？公式？
+ */
+public class Top100_96 {
+
+    /**
+     * n=1，res = 1
+     * n=2,res = 2
+     * n=3,res =
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        return n;
+    }
+}
