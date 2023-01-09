f84cbf9bc8ccac7f0095b7ece0733d50a98df80c
jiujiuwo <lihaifeng94@qq.com>
2023-01-09 14:15:09 +0800

    动态规划法，解决不同二叉搜索树问题，是不是可以用递归的方法解决？

--- src/top100/Top100_96.java
+++ src/top100/Top100_96.java
@@ -10,10 +10,22 @@ public class Top100_96 {
      * n=1，res = 1
      * n=2,res = 2
      * n=3,res =
+     * 动态规划，找规律？
+     *
      * @param n
      * @return
      */
     public int numTrees(int n) {
-        return n;
+
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
     }
 }
