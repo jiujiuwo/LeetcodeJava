ea2325a1d3e1355f8b7160d09784dc50bba3da47
jiujiuwo <lihaifeng94@qq.com>
2023-01-09 14:31:52 +0800

    思考第二种解法

--- src/top100/Top100_560.java
+++ src/top100/Top100_560.java
@@ -30,4 +30,27 @@ public class Top100_560 {
         }
         return result;
     }
+
+    /**
+     * 第二种解法？动态规划？栈？还是什么？hash?
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int subarraySum1(int[] nums, int k) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                //找出所有子数组
+                int sum = 0;
+                for (int m = i; m <= j; m++) {
+                    sum += nums[m];
+                }
+                if (sum == k) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
 }
