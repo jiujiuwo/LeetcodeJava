065c772da9c45e0eb030b3217bef626f54b1194a
jiujiuwo <lihaifeng94@qq.com>
2023-01-11 13:48:20 +0800

    二叉树问题，求解思路？遍历？

---
+++ src/top100/Top100_236.java
@@ -0,0 +1,29 @@
+package top100;
+
+/**
+ * 二叉树的最近公共祖先
+ */
+public class Top100_236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return root;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
