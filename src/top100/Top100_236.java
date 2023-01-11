1912d46aa3cd9718fad7378cdc3490524fbe89f4
jiujiuwo <lihaifeng94@qq.com>
2023-01-11 16:23:17 +0800

    二叉树问题，求解思路？遍历？
    递归的思路求解。

--- src/top100/Top100_236.java
+++ src/top100/Top100_236.java
@@ -4,15 +4,26 @@ package top100;
  * 二叉树的最近公共祖先
  */
 public class Top100_236 {
+
+    /**
+     * 递归的做法，别人的做法，怎么变成自己的？
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-
-        return root;
+        if (root == p || root == q || root == null) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        // 如果left 和 right都不为空，说明此时root就是最近公共节点
+        // 如果left为空，right不为空，就返回right，说明目标节点是通过right返回的，反之亦然。
+        if (left != null && right != null) return root;
+        if (left == null) return right;
+        return left;
     }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
 }
 
 /**
