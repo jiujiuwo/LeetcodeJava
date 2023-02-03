857ed6f3bf4655184ef9b8297507e89713a40bcd
jiujiuwo <lihaifeng94@qq.com>
2023-02-03 13:32:27 +0800

    判断是否是环形链表，双指针做法

--- src/top100/Top100_142.java
+++ src/top100/Top100_142.java
@@ -29,6 +29,13 @@ public class Top100_142 {
      * }
      */
     public class Solution {
+
+        /**
+         * 使用hash,或者这种链表的方法来做，可以通过
+         *
+         * @param head
+         * @return
+         */
         public ListNode detectCycle(ListNode head) {
             List<ListNode> nodes = new ArrayList<>();
             ListNode ptr = head;
@@ -43,4 +50,28 @@ public class Top100_142 {
         }
     }
 
+    /**
+     * 快慢指针做法
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle2(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        // 先找出第一次相遇的位置，此时fast和slow分别走了 n 和 2n次环
+        while (true) {
+            if (fast == null || fast.next == null) return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return fast;
+    }
 }
+
