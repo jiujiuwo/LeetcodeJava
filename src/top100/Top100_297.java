e2ac52e2b1ead92e2f6cbe662069921915895d9d
jiujiuwo <lihaifeng94@qq.com>
2023-02-03 14:38:05 +0800

    二叉树，序列化与反序列化

---
+++ src/top100/Top100_297.java
@@ -0,0 +1,33 @@
+package top100;
+
+/**
+ * 序列化和反序列化二叉树
+ */
+public class Top100_297 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Codec {
+
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+            return "";
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            return null;
+        }
+    }
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
+}
